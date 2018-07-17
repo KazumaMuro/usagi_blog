3eca8b0e8bf830a2862b88d9fc1c6ce84a3bc905
室和真 <muu66kanata@murowashinnoMacBook-puro.local>
2018-07-17 23:01:34 +0900

    Finish blog

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,12 +1,3 @@
 class Article < ApplicationRecord
   mount_uploader :picture, PictureUploader
-
-  # def before_article
-  #   Article.order(created_at: :desc).where("created_at <= ?", created_at).first
-  # end
-  #
-  # def next_article
-  #   Article.order(created_at: :asc).where("created_at <= ?", created_at).first
-  # end
-
 end
